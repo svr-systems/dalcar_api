af33270b90f29d4a7b1eac1f59c9798f3604b30a
Er Leví <ivelre123@gmail.com>
2025-09-29 22:36:50 -0600

    Legacy vehicle investors

---
+++ app/Http/Controllers/LegacyVehicleInvestorController.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\LegacyVehicleInvestor;
+use DB;
+use Illuminate\Http\Request;
+use Throwable;
+
+class LegacyVehicleInvestorController extends Controller {
+  public function index(Request $req) {
+    try {
+      return $this->apiRsp(
+        200,
+        'Registros retornados correctamente',
+        ['items' => LegacyVehicleInvestor::getItems($req)]
+      );
+    } catch (Throwable $err) {
+      return $this->apiRsp(500, null, $err);
+    }
+  }
+
+  public function show(Request $req, $id) {
+    try {
+      return $this->apiRsp(
+        200,
+        'Registro retornado correctamente',
+        ['item' => LegacyVehicleInvestor::getItem($req, $id)]
+      );
+    } catch (Throwable $err) {
+      return $this->apiRsp(500, null, $err);
+    }
+  }
+
+  public function destroy(Request $req, $id) {
+    DB::beginTransaction();
+    try {
+      $item = LegacyVehicleInvestor::find($id);
+
+      if (!$item) {
+        return $this->apiRsp(422, 'ID no existente');
+      }
+
+      $item->is_active = false;
+      $item->updated_by_id = $req->user()->id;
+      $item->save();
+
+      DB::commit();
+      return $this->apiRsp(
+        200,
+        'Registro inactivado correctamente'
+      );
+    } catch (Throwable $err) {
+      DB::rollback();
+      return $this->apiRsp(500, null, $err);
+    }
+
+  }
+
+  public function store(Request $req) {
+    return $this->storeUpdate($req, null);
+  }
+
+  public function update(Request $req, $id) {
+    return $this->storeUpdate($req, $id);
+  }
+
+  public function storeUpdate($req, $id) {
+    DB::beginTransaction();
+    try {
+
+      $valid = LegacyVehicleInvestor::valid($req->all());
+
+      if ($valid->fails()) {
+        return $this->apiRsp(422, $valid->errors()->first());
+      }
+
+      $store_mode = is_null($id);
+
+      if ($store_mode) {
+        $item = new LegacyVehicleInvestor;
+        $item->created_by_id = $req->user()->id;
+        $item->updated_by_id = $req->user()->id;
+      } else {
+        $item = LegacyVehicleInvestor::find($id);
+        $item->updated_by_id = $req->user()->id;
+      }
+
+      $item = $this->saveItem($item, $req);
+
+      DB::commit();
+      return $this->apiRsp(
+        $store_mode ? 201 : 200,
+        'Registro ' . ($store_mode ? 'agregado' : 'editado') . ' correctamente',
+        $store_mode ? ['item' => ['id' => $item->id]] : null
+      );
+    } catch (Throwable $err) {
+      DB::rollback();
+      return $this->apiRsp(500, null, $err);
+    }
+  }
+
+  public static function saveItem($item, $data, $is_req = true) {
+    if (!$is_req) {
+      $item->active = GenController::filter($data->active, 'b');
+    }
+
+    $item->legacy_vehicle_id = GenController::filter($data->legacy_vehicle_id, 'id');
+    $item->investor_id = GenController::filter($data->investor_id, 'id');
+    $item->percentages = GenController::filter($data->percentages, 'f');
+    $item->amount = GenController::filter($data->amount, 'f');
+
+    $item->save();
+
+    return $item;
+  }
+}
--- app/Models/LegacyVehicleInvestor.php
+++ app/Models/LegacyVehicleInvestor.php
@@ -2,11 +2,86 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
+use DateTimeInterface;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Validator;
 
 class LegacyVehicleInvestor extends Model
 {
-  use HasFactory;
-  public $timestamps = false;
+  protected function serializeDate(DateTimeInterface $date) {
+    return Carbon::instance($date)->toISOString(true);
+  }
+  protected $casts = [
+    'created_at' => 'datetime:Y-m-d H:i:s',
+    'updated_at' => 'datetime:Y-m-d H:i:s',
+  ];
+
+  public static function valid($data, $is_req = true) {
+    $rules = [
+      'legacy_vehicle_id' => 'required|numeric',
+      'investor_id' => 'required|numeric',
+      'percentages' => 'required|numeric',
+      'amount' => 'required|numeric',
+    ];
+
+    if (!$is_req) {
+      array_push($rules, ['is_active' => 'required|in:true,false,1,0']);
+    }
+
+    $msgs = [];
+
+    return Validator::make($data, $rules, $msgs);
+  }
+
+  static public function getUiid($id) {
+    return 'LVI-' . str_pad($id, 4, '0', STR_PAD_LEFT);
+  }
+
+  static public function getItems($req) {
+    $items = LegacyVehicleInvestor::
+      where('legacy_vehicle_id', $req->legacy_vehicle_id)->
+      where('is_active', boolval($req->is_active));
+
+    $items = $items->
+    get([
+        'id',
+        'is_active',
+        'percentages',
+        'amount',
+      ]);
+
+    foreach ($items as $key => $item) {
+      $item->key = $key;
+      $item->uiid = LegacyVehicleInvestor::getUiid($item->id);
+    }
+
+    return $items;
+  }
+
+  static public function getItem($req, $id) {
+    $item = LegacyVehicleInvestor::
+      where('id', $id)->
+      find($id, [
+        'id',
+        'is_active',
+        'created_at',
+        'updated_at',
+        'created_by_id',
+        'updated_by_id',
+        'investor_id',
+        'percentages',
+        'amount',
+      ]);
+
+    if ($item) {
+      $item->created_by = User::find($item->created_by_id, ['email']);
+      $item->updated_by = User::find($item->updated_by_id, ['email']);
+      $item->investor = Investor::find($item->investor_id);
+      $item->uiid = LegacyVehicleInvestor::getUiid($item->id);
+    }
+
+    return $item;
+  }
 }
